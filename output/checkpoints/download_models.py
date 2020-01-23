43630a89614c2ada5a00f18632bac7fb4a4e6498
hytseng <hytseng0509@gmail.com>
2020-01-22 22:46:09 -0800

    download model

--- output/checkpoints/download_models.py
+++ output/checkpoints/download_models.py
@@ -1,15 +1,19 @@
 from subprocess import call
 import sys
 
-if len(sys.argv) != 5:
-  raise Exception('Incorrect command! e.g., python3 process.py [single/multi] [miniImagenet/cub/cars/places/plantae] [ori/fa/lft] [matchingnet/relationnet/gnnnet]')
+# current available models:
+# multi cars 1 ori gnnnet
+
+if len(sys.argv) != 6:
+  raise Exception('Incorrect command! e.g., python3 download_models.py [single/multi] [miniImagenet/cub/cars/places/plantae] [1/5] [ori/fa/lft] [matchingnet/relationnet/gnnnet]')
 
 task = sys.argv[1]
 testset = sys.argv[2]
-method = sys.argv[3]
-model = sys.argv[4]
+shot = sys.argv[3]
+method = sys.argv[4]
+model = sys.argv[5]
 
-filename = task + '_' + testset + '_' + method + '_' + model + '.tar.gz'
+filename = task + '_' + testset + '_' + shot + '_' + method + '_' + model + '.tar.gz'
 
 call('wget http://vllab.ucmerced.edu/ym41608/projects/CrossDomainFewShot/checkpoints/' + filename, shell=True)
 call('tar -zxf ' + filename, shell=True)
